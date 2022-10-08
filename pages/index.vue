dc6fe9862a1dc3e69ab99ede90d8b465726da072
KK-mp4 <livinglegendilmumbo@gmail.com>
2022-10-08 21:23:12 +0300

    feat: added threshold, and changed menu

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,13 @@ useHead({ title: "Generator" });
 const textureResolution = ref("16x16");
 const src = ref("");
 let textureSize = 16;
-let maxSize = ref(512);
+const maxSize = ref(512);
 let inputUrl;
+const dithering = ref("None");
+const threshold = ref(128);
 
 async function Draw() {
+  const start = Date.now();
   const img = new Image();
   img.src = inputUrl;
   await new Promise((resolve) => {
@@ -50,9 +53,9 @@ async function Draw() {
 
   for (let i = 0; i < imgHeight; i++) {
     for (let j = 0; j < imgWidth; j++) {
-      const color = imgContext.getImageData(j, i, 1, 1).data;
-      const brightness = 0.2126 * color[0] + 0.7152 * color[1] + 0.0722 * color[2];
-      if (brightness >= 0.5) {
+      const pixelRGBA = imgContext.getImageData(j, i, 1, 1).data;
+      const pixelBrightness = 0.2126 * pixelRGBA[0] + 0.7152 * pixelRGBA[1] + 0.0722 * pixelRGBA[2];
+      if (pixelBrightness >= threshold.value) {
         ctx.drawImage(lampOn, j * textureSize, i * textureSize);
       } else {
         ctx.drawImage(lampOff, j * textureSize, i * textureSize);
@@ -61,6 +64,7 @@ async function Draw() {
   }
 
   src.value = canvas.toDataURL();
+  console.log(Date.now() - start + "ms - Calculation time");
 }
 
 async function UploadImg(event) {
@@ -109,15 +113,33 @@ async function ResolutionChane() {
   <div class="flex justify-center flex-wrap pt-2 flex-col items-center min-w-[300px]">
     <p class="text-lg text-gray-300">Lamp Display Emulator by KK</p>
     <a class="mb-5 text-xs text-blue-400 underline" href="https://github.com/KK-mp4/Lamp-Display-Emulator#readme" target="_blank" rel="noopener noreferrer">More info in GitHub</a>
-    <select v-model="textureResolution" class="w-64 mb-5 rounded-lg border h-7 bg-gray-700 border-gray-600 text-gray-400" @change="ResolutionChane()">
-      <option value="1x1">1x1</option>
-      <option value="2x2">2x2</option>
-      <option value="4x4">4x4</option>
-      <option value="8x8">8x8</option>
-      <option value="16x16">16x16</option>
-    </select>
-    <input class="block text-sm text-gray-400 rounded-lg border cursor-pointer focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400" type="file" accept=".png, .jpg" @change="UploadImg($event)">
-    <p class="mt-1 text-sm text-gray-300">PNG or JPG ({{ maxSize }}x{{ maxSize }}px max)</p>
+      <div class="flex flex-wrap flex-row justify-center">
+      <div class="w-[300px]">
+        <p class="mb-1 text-sm text-gray-300">Lamp texture resolution:</p>
+        <select v-model="textureResolution" class="w-64 mb-5 rounded-lg border h-7 bg-gray-700 border-gray-600 text-gray-400" @change="ResolutionChane()">
+          <option value="1x1">1x1</option>
+          <option value="2x2">2x2</option>
+          <option value="4x4">4x4</option>
+          <option value="8x8">8x8</option>
+          <option value="16x16">16x16</option>
+        </select>
+        <p class="mb-1 text-sm text-gray-300">PNG or JPG ({{ maxSize }}x{{ maxSize }}px max)</p>
+        <input class="block text-sm text-gray-400 rounded-lg border cursor-pointer focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400" type="file" accept=".png, .jpg" @change="UploadImg($event)">
+      </div>
+      <div class="w-[300px]">
+        <p class="mb-1 text-sm text-gray-300">Dithering algorithm:</p>
+        <select v-model="dithering" class="w-64 mb-5 rounded-lg border h-7 bg-gray-700 border-gray-600 text-gray-400" @change="ResolutionChane()">
+          <option value="None">None</option>
+          <!-- <option value="Random noise">Random noise</option>
+          <option value="Bayer 2x2">Bayer 2x2</option>
+          <option value="Bayer 4x4">Bayer 4x4</option>
+          <option value="Bayer 8x8">Bayer 8x8</option>
+          <option value="Floyd-Steinberg">Floyd-Steinberg</option> -->
+        </select>
+        <label class="block mb-2 text-sm font-medium text-gray-300">Threshold: {{  threshold }}</label>
+        <input v-model="threshold" type="range" min="0" max="255" class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700" @change="ResolutionChane()">
+      </div>
+    </div>
     <div
       class="w-full h-full mt-5 flex justify-center"
       style="image-rendering: pixelated"
